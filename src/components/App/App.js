87309152407b2f97e8baa18aa2f86830c9419e24
StevenTheBearded <>
2021-07-26 13:07:53 -0400

    Import apicall function

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,11 +1,25 @@
 import React, { useState } from 'react';
+import { getTips } from '../../ApiCalls'
+
 
 const App = () => {
   const [advice, setAdvice] = useState([]);
 
+  
+  //function to call and set the state to the apicall
+
+  
+
+  useEffect(() => {
+    getTips()
+  }, [])
+
   return (
     <main className='main'>
-      <TipJar />
+      {/* <TipJar /> */}
     </main>
   )
+
 };
+
+export default App
