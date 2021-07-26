02e8cfca8640bbf60864d84dc747366e70e98186
StevenTheBearded <unknown>
2021-07-26 13:28:06 -0400

    Functionality to set state off apicall

--- src/ApiCalls.js
+++ src/ApiCalls.js
@@ -1,13 +1,5 @@
-export const getTips = async () => {
-  const url = 'https://turingtwocentapi.herokuapp.com/'
-  // setError('')
-
-  try {
-    const response = await fetch(url)
-    const tips = await response.json()
-    // setPets(tips)
-    console.log(tips, "<<<L>> tips inside apicalls")
-  } catch(error) {
-    // setError(error.message)
-  }
+export const getTips = () => {
+  return fetch('https://turingtwocentapi.herokuapp.com/')
+    .then(response => response.json())
+    .catch(err => console.log(err))
 }
