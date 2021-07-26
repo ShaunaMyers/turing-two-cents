4bf1c5adb255ed47607965071e26fc84025732d7
StevenTheBearded <>
2021-07-26 13:07:20 -0400

    Create function to export apicalls

---
+++ src/ApiCalls.js
@@ -0,0 +1,13 @@
+export const getTips = async () => {
+  const url = 'https://turingtwocentapi.herokuapp.com/'
+  setError('')
+
+  try {
+    const response = await fetch(url)
+    const tips = await response.json()
+    // setPets(tips)
+    console.log(tips, "<<<L>> tips inside apicalls")
+  } catch(error) {
+    setError(error.message)
+  }
+}
