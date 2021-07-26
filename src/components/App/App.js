74629010039383e8d4bc8fa0d44cfcb178538f3f
StevenTheBearded <>
2021-07-26 13:48:08 -0400

    Add TipJar to return

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,18 +1,19 @@
 import React, { useState, useEffect } from 'react';
 import { getTips } from '../../ApiCalls'
+import TipJar from '../TipJar/TipJar'
 
 
 const App = () => {
   const [advice, setAdvice] = useState([]);
 
   useEffect(() => {
-    getTips().then(data => setAdvice(data))
+    getTips().then(data => setAdvice(data.rows))
   }, [])
 
   return (
     <main className='main'>
-      {/* <TipJar /> */}
-      <h1>HI WE EXIST</h1>
+      <header><h1>HI WE EXIST</h1></header>
+      <TipJar tips={ advice } />
     </main>
   )
 };
