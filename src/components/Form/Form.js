8c090854ffd0a36e3e93a3c7f16e341b5487fac4
Shauna Myers <shaunamyers26@gmail.com>
2021-07-26 20:30:49 +0100

    Add method to update description on change

--- src/components/Form/Form.js
+++ src/components/Form/Form.js
@@ -4,12 +4,20 @@ const Form = () => {
     const [title, setTitle] = useState('');
     const [description, setDescription] = useState('');
 
+    const handleTitle = (e) => {
+        setTitle(e.target.value);
+        console.log(title, "THIS IS OUR TITLE");
+    }
 
+    const handleDescription = (e) => {
+        setDescription(e.target.value);
+        console.log(description, "THIS IS OUR Description")
+    }
 
     return(
         <form>
-            <input onChange={handleTitle} type="text" placeholder="Tip Title"/>
-            <input onChange={handleDescription} type="text" placeholder="Description"/>
+            <input onChange={handleTitle} type="text" placeholder="Tip Title" value={title}/>
+            <input onChange={handleDescription} type="text" placeholder="Description" value={description}/>
             <select>
                 <option value="1">Mod 1</option>
                 <option value="2">Mod 2</option>
