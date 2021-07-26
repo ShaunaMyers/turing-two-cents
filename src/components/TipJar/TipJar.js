01053ad4a1d80bb0978dd92cff10299f9644ade6
StevenTheBearded <>
2021-07-26 16:43:38 -0400

    remove comments from tipjar

--- src/components/TipJar/TipJar.js
+++ src/components/TipJar/TipJar.js
@@ -6,15 +6,16 @@ import './TipJar.css';
 const TipJar = ({ tips }) => {
   console.log(tips,' :tips inside tipjar.jcs <<<<<')
   const allTips = tips.map(tip => {
+    console.log(tip.id, ' :tips.id inside TipJar.js');
     return (
       <TipCard 
         id={tip.id}
+        key={tip.id}
         title={tip.title}
         description={tip.description}
         mod={tip.mod}
         upvotes={tip.upvotes}
         date={tip.date}
-        key={tip.id}
       />
     )
   });
