972a6e2e60ce2aedf1d979a4e4926ca52adaeb46
StevenTheBearded <>
2021-07-26 13:29:00 -0400

    Delete comments

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -5,18 +5,6 @@ import { getTips } from '../../ApiCalls'
 const App = () => {
   const [advice, setAdvice] = useState([]);
 
-  
-  //function to call and set the state to the apicall
-
-  // .then(data = data => { 
-  //   this.setState({advice})
-  // })
-  
-  // const removeCard = (id) => {
-  //   let filteredCards = cards.filter(card => card.id !== id);
-  //   setCards(filteredCards);
-  // }
-
   useEffect(() => {
     getTips().then(data => setAdvice(data))
   }, [])
