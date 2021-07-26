15a3bf5e31a76d7f2ec84d22429f597cf9fd5043
StevenTheBearded <unknown>
2021-07-26 13:38:25 -0400

    Create allTips function

--- src/components/TipJar/TipJar.js
+++ src/components/TipJar/TipJar.js
@@ -1,1 +1,27 @@
-import React
+import { description } from 'commander';
+import React from 'react';
+import TipCard from '../TipCard/TipCard'
+import './TipJar.css';
+
+const TipJar = ({ tips }) => {
+  const allTips = tips.map(tip => {
+    return (
+      <TipCard 
+        id={tip.id}
+        title={tip.title}
+        description={tip.description}
+        mod={tip.mod}
+        upvotes={tip.upvotes}
+        date={tip.date}
+      />
+    )
+  });
+
+    return (
+      <section className='tip-jar'>
+        {allTips}
+      </section>
+    )
+};
+
+export default TipJar;
