64176bf8b598828e50adc6125acc62ee679c4c31
Taylor Galloway <gallowaytaylor@outlook.com>
2021-07-28 10:05:20 -0600

    Change upvote to rating

--- src/components/TipCard/TipCard.js
+++ src/components/TipCard/TipCard.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import './TipCard.css'
 
-const Tip = ({ id, title, description, mod, upvotes, date}) => {
+const Tip = ({ id, title, description, mod, rating, date}) => {
   return (
     <article className='tip-card'>
       <h2>{title}</h2>
       <p className="description">{description}</p>
       <p>{mod}</p>
-      <p>{upvotes}</p>
+      <p>{rating}</p>
       <p>{date}</p>
     </article>
   )
