e255559e621b9f8eb74843810c202932145c8c98
StevenTheBearded <stevenberg22@gmail.com>
2021-07-29 10:45:47 -0400

    Add test for viewing of all cards

--- cypress/integration/homepage_spec.js
+++ cypress/integration/homepage_spec.js
@@ -9,7 +9,7 @@ describe('Home Page', () => {
     cy.get('.nav-header').contains('Turing Tip Jar')
     cy.get('form').should('be.visible')
   })
-  it('Should not add card if all inputs are not filled out', () => {
+  it('Should not populate card if all inputs are not filled out', () => {
     cy.get('.new-tip-button').click()
     cy.get(':nth-child(3) > p').contains('Please fill out title & description fields.')
   })
@@ -27,16 +27,18 @@ describe('Home Page', () => {
     })
   it('As a user, I should be able to click on Module 1 and see only cards pertaining to Module 1', () => {
     cy.get('[href="/module/1"]').click()
-    cy.get('h2').contains('Title for mod1')
-  })
-  it('As a user, I should be able to click on Show All and see all the cards', () => { 
-    cy.get('[href="/module/1"]').click()
-    cy.get('.nav-header > :nth-child(6)').click()
-    cy.get('.tip-jar > :nth-child(3)')
+    cy.get('h2').contains('How to Duke')
   })
   it('As a user, i should be able to click Back Button after clicking Module 1 to return to Home Page', () => {
     cy.get('[href="/module/1"]').click()
     cy.go('back')
     cy.url().should('eq','http://localhost:3000/')
+  })
+  it('As a user, I should be able to click on Show All and see all the cards', () => { 
+    cy.get('[href="/module/1"]').click()
+    cy.get('.nav-header > :nth-child(6)').click()
+    cy.get('.tip-jar')
+      .children('.tip-card')
+      .should('have.length', 3)
   })
 })
